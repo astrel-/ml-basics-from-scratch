b71700f0584b27ea29d62df0a2d84c20494056ff
Yury <yury.baryshnikov137@gmail.com>
2026-03-07 12:13:38 +0000

    Added faster vectorized pairwise distance calculation using matrix cross product

--- methods/kNN/python/kNN.py
+++ methods/kNN/python/kNN.py
@@ -1,13 +1,38 @@
+from enum import Enum
 import numpy as np
 
 class DistanceCalculator:
+
+    class Method(Enum):
+        ThreeDBroadcasting = 1
+        CrossProduct = 2
+
+    @staticmethod 
+    def calc_distance_sq(A: np.ndarray, B: np.ndarray, method=Method.ThreeDBroadcasting) -> np.ndarray:
+        if method == DistanceCalculator.Method.ThreeDBroadcasting:
+            return DistanceCalculator.calc_distance_sq_3d_broadcasting(A, B)
+
+        if method == DistanceCalculator.Method.CrossProduct:
+            return DistanceCalculator.calc_distance_sq_cross_prod(A, B)
+        
+        raise RuntimeError("Unrecognized Method passed")
+
     @staticmethod
-    def calc_distance(x: np.ndarray, other: np.ndarray) -> np.ndarray:
-        distances = np.sqrt(np.sum((x[:, None, :] - other[None, :, :]) **2, axis=2))
+    def calc_distance_sq_3d_broadcasting(x: np.ndarray, other: np.ndarray) -> np.ndarray:
+        distances = np.sum((x[:, None, :] - other[None, :, :]) **2, axis=2)
+        return distances
+    
+    @staticmethod
+    def calc_distance_sq_cross_prod(A: np.ndarray, B: np.ndarray) -> np.ndarray:
+        ''' doesn't rely on 3d-broadcasting '''
+        cross_prod = -2.0 * A @ B.T
+        A_sq = np.sum(A**2, axis=1, keepdims=True)
+        B_sq = np.sum(B**2, axis=1, keepdims=True)
+        distances = cross_prod + A_sq + B_sq.T
         return distances
 
 
-class CustomKNeighborsClassifier:
+class CustomVectorizedKNeighborsClassifier:
     ''' - Brute force approach;
     - uses partition (np.argpartition) to find k nearest neighbours, does not sort them in order, 
     which makes it more efficient for a single value of k'''
@@ -22,11 +47,11 @@ class CustomKNeighborsClassifier:
         self.y_min = y_train.min()
 
     @staticmethod
-    def calc_distance(x: np.ndarray, other: np.ndarray) -> np.ndarray:
-        return DistanceCalculator.calc_distance(x, other)
+    def calc_distance(x: np.ndarray, other: np.ndarray, method) -> np.ndarray:
+        return DistanceCalculator.calc_distance_sq(x, other, method)
 
-    def predict(self, x: np.ndarray) -> np.ndarray:
-        distances = self.calc_distance(x, self.x_train)
+    def predict(self, x: np.ndarray, method=DistanceCalculator.Method.CrossProduct) -> np.ndarray:
+        distances = self.calc_distance(x, self.x_train, method)
         
         min_distances_args = np.argpartition(distances, self.n_neighbors, axis=1)[:, :self.n_neighbors]
         classes_of_nearest_neighbours = self.y_train[min_distances_args]
@@ -40,9 +65,9 @@ class CustomKNeighborsClassifier:
         pred = self.y_min + np.argmax(counts, axis=1)
 
         return pred
-    
 
-class CustomSortedKNeighborsClassifier:
+    
+class CustomVectorizedSortedKNeighborsClassifier:
     ''' - Brute force approach;
     - sorts neighbours by distance, which can be reused for multiple values of K '''
 
@@ -56,11 +81,11 @@ class CustomSortedKNeighborsClassifier:
         self.y_min = y_train.min()
 
     @staticmethod
-    def calc_distance(x: np.ndarray, other: np.ndarray) -> np.ndarray:
-        return DistanceCalculator.calc_distance(x, other)
+    def calc_distance(x: np.ndarray, other: np.ndarray, method) -> np.ndarray:
+        return DistanceCalculator.calc_distance_sq(x, other, method)
 
-    def predict(self, x: np.ndarray) -> np.ndarray:
-        distances = self.calc_distance(x, self.x_train)
+    def predict(self, x: np.ndarray, method=DistanceCalculator.Method.CrossProduct) -> np.ndarray:
+        distances = self.calc_distance(x, self.x_train, method)
         
         min_distances_args = np.argsort(distances, axis=1)[:, :self.n_neighbours]
         classes_of_nearest_neighbours = self.y_train[min_distances_args]
