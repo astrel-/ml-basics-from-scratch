f3ba3d11ae18d1e426c885c5808b34be727c53d4
Yury <yury.baryshnikov137@gmail.com>
2026-05-17 21:25:12 +0100

    separating io/numpy

---
+++ src/io/numpy.cpp
@@ -0,0 +1,69 @@
+#include <io/numpy.h>
+#include <cnpy.h>
+#include <containers/matrix.h>
+#include <cstdint>
+#include <stdexcept>
+#include <vector>
+#ifdef KNN_USE_TORCH
+#include <torch/torch.h>
+#endif
+
+namespace io::numpy {
+
+containers::XY readXY(const char *filePath) {
+    auto data = cnpy::npz_load(filePath);
+    auto xArr = read2D(data["X"]);
+    auto yArr = read1D(data["y"]);
+    return {xArr, yArr};
+}
+
+containers::matrix read2D(const cnpy::NpyArray &array) {
+    // consider avoiding copy later
+    if (array.fortran_order)
+        throw std::runtime_error("Only C order is supported at the moment");
+    if (array.shape.size() != 2)
+        throw std::runtime_error("Must be a 2D-array");
+    if (array.word_size != sizeof(double))
+        throw std::runtime_error("Array does not contain doubles");
+
+    auto buffer = array.as_vec<double>();
+    auto rows = array.shape[0];
+    auto cols = array.shape[1];
+    return containers::matrix(buffer, rows, cols);
+}
+
+containers::Vector1D read1D(const cnpy::NpyArray &array) {
+    const auto &shape = array.shape;
+    if (shape.size() > 2)
+        throw std::runtime_error("Must be either 2D-array or 1D-array");
+    else if (shape.size() == 2) {
+        if ((shape[0] != 1) && (shape[1] != 1))
+            throw std::runtime_error("If 2D-array is supplied, it must be of shape (n,1) or (1,n)");
+    }
+    if (array.word_size != sizeof(std::int8_t))
+        throw std::runtime_error("Array does not contain int8_t");
+
+    return array.as_vec<std::int8_t>();
+}
+} // namespace io::numpy
+
+#ifdef KNN_USE_TORCH
+namespace io::numpy::tensor {
+torch::Tensor toTensorDouble(containers::matrix &m) {
+    return torch::from_blob(m.data(), {static_cast<int64_t>(m.rows), static_cast<int64_t>(m.cols)},
+                            torch::kFloat64)
+        .clone();
+}
+
+torch::Tensor toTensorInt8(containers::Vector1D &v) {
+    return torch::from_blob(v.data(), {static_cast<int64_t>(v.size())}, torch::kInt8).clone();
+}
+
+struct XY readXY(const char *filePath) {
+    auto [X, y] = io::numpy::readXY(filePath);
+    auto xTs = toTensorDouble(X);
+    auto yTs = toTensorInt8(y);
+    return {xTs, yTs};
+}
+} // namespace io::numpy::tensor
+#endif
